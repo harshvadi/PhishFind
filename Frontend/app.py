a472581834b808bf2a3b7a745fd1695108e1c853
harshvadi07 <harshvadi12345@gmail.com>
2023-05-01 13:48:09 +0530

    dl models added

--- Frontend/app.py
+++ Frontend/app.py
@@ -11,9 +11,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/urlPage")
+@app.route("/urlPage", methods=["GET"])
 def urlPage():
-    return render_template("urlPage.html")
+    return render_template("urlPage.html", prediction=-1)
 
 
 @app.route("/urlPage", methods=["POST"])
@@ -33,17 +33,45 @@ def submit():
     tree = joblib.load("./prediction/tree.pkl")
     prediction_tree = tree.predict(pd.read_csv("test.csv"))
 
+    cnn = joblib.load("./prediction/cnn.pkl")
+    prediction_cnn = cnn.predict(pd.read_csv("test.csv"))
+
+    rnn = joblib.load("./prediction/rnn.pkl")
+    prediction_rnn = rnn.predict(pd.read_csv("test.csv"))
+
+    lstm = joblib.load("./prediction/lstm.pkl")
+    prediction_lstm = lstm.predict(pd.read_csv("test.csv"))
+
+    if prediction_cnn[0][0] <= 0.5:
+        prediction_cnn[0][0] = int(0)
+    else:
+        prediction_cnn[0][0] = int(1)
+
+    if prediction_rnn[0][0] <= 0.5:
+        prediction_rnn[0][0] = int(0)
+    else:
+        prediction_rnn[0][0] = int(1)
+
+    if prediction_lstm[0][0] <= 0.5:
+        prediction_lstm[0][0] = int(0)
+    else:
+        prediction_lstm[0][0] = int(1)
+
     prediction = (
         prediction_knn[0]
         + prediction_svm[0]
         + prediction_forest[0]
         + prediction_tree[0]
+        + int(prediction_cnn[0][0])
+        + int(prediction_rnn[0][0])
+        + int(prediction_lstm[0][0])
     )
 
     print(prediction)
 
-    # majority voting
-    # prediction = prediction / 2
+    prediction = int(prediction / 4)
+
+    print(prediction)
 
     return render_template("urlPage.html", prediction=prediction, url=url)
 
